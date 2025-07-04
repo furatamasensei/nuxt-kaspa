5c7133cf84bf686aaaf3b109d9fba1fd4745b207
Donny Pratama <pratamatama1024@gmail.com>
2025-07-05 04:00:36 +0700

    feat(playground): add empty message to terminal

--- playground/components/Terminal.vue
+++ playground/components/Terminal.vue
@@ -48,8 +48,9 @@ const togglePause = () => {
         />
       </div>
     </template>
-    <div class="min-h-[435px] max-w-full text-xs p-4">
+    <div class="h-[435px] max-w-full p-4">
       <DynamicScroller
+        v-if="model.length"
         ref="scrollerRef"
         :items="model"
         :min-item-size="24"
@@ -62,12 +63,25 @@ const togglePause = () => {
             :active="active"
             :data-index="index"
           >
-            <div class="whitespace-nowrap font-mono text-primary-500">
+            <div class="whitespace-nowrap font-mono text-primary-500 text-xs">
               {{ render(item) }}
             </div>
           </DynamicScrollerItem>
         </template>
       </DynamicScroller>
+      <div v-else class="h-full flex flex-col items-center justify-center">
+        <UIcon
+          name="i-lucide-triangle-alert"
+          class="text-yellow-500 text-4xl"
+        />
+        <div class="text-neutral mt-2 max-w-lg mx-auto text-center text-sm">
+          If you see this, it means that there are no events to display.
+          <br />
+          The blockchain might not have emitted any events yet.
+          <br />
+          This is not a bug.
+        </div>
+      </div>
     </div>
   </UCard>
 </template>
