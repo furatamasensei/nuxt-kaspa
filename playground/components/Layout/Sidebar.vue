74d1c64b376fc07a32e77deb1c2c3ecba28ea3de
Donny Pratama <pratamatama1024@gmail.com>
2025-07-04 19:05:36 +0700

    feat: better playground layout

--- playground/components/Layout/Sidebar.vue
+++ playground/components/Layout/Sidebar.vue
@@ -1,66 +1,65 @@
 <script setup lang="ts">
-const demoItems = [
-  {
-    to: '/subscriptions/block-added',
-    title: 'useSubscribeBlockAdded',
-    description: 'Listen to block-added event',
-  },
-  {
-    to: '/subscriptions/finality-conflict',
-    title: 'useSubscribeFinalityConflict',
-    description: 'Listen to finality-conflict event',
-  },
-  {
-    to: '/subscriptions/finality-conflict-resolved',
-    title: 'useSubscribeFinalityConflictResolved',
-    description: 'Listen to finality-conflict-resolved event',
-  },
-  {
-    to: '/subscriptions/new-block-template',
-    title: 'useSubscribeNewBlockTemplate',
-    description: 'Listen to new-block-template event',
-  },
-  {
-    to: '/subscriptions/pruning-point-utxo-set-override',
-    title: 'useSubscribePruningPointUtxoSetOverride',
-    description: 'Listen to pruning-point-utxo-set-override event',
-  },
-  {
-    to: '/subscriptions/sink-blue-score-changed',
-    title: 'useSinkBlueScoreChanged',
-    description: 'Listen to sink-blue-score-changed event',
-  },
-  {
-    to: '/subscriptions/utxos-changed',
-    title: 'useSubscribeUtxosChanged',
-    description: 'Listen to utxos-changed event',
-  },
-  {
-    to: '/subscriptions/virtual-chain-changed',
-    title: 'useSubscribeVirtualChainChanged',
-    description: 'Listen to virtual-chain-changed event',
-  },
-  {
-    to: '/subscriptions/virtual-daa-score-changed',
-    title: 'useSubscribeVirtualDaaScoreChanged',
-    description: 'Listen to virtual-daa-score-changed event',
-  },
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items: NavigationMenuItem[][] = [
+  [
+    {
+      label: 'Event Listener',
+      type: 'label',
+      icon: 'i-lucide-rss',
+      active: true,
+      defaultOpen: true,
+      children: [
+        {
+          label: 'useSubscribeBlockAdded',
+          to: '/subscriptions/block-added',
+        },
+        {
+          label: 'useSubscribeFinalityConflict',
+          to: '/subscriptions/finality-conflict',
+        },
+        {
+          label: 'useSubscribeFinalityConflictResolved',
+          to: '/subscriptions/finality-conflict-resolved',
+        },
+        {
+          label: 'useSubscribeNewBlockTemplate',
+          to: '/subscriptions/new-block-template',
+        },
+        {
+          label: 'useSubscribePruningPointUtxoSetOverride',
+          to: '/subscriptions/pruning-point-utxo-set-override',
+        },
+        {
+          label: 'useSinkBlueScoreChanged',
+          to: '/subscriptions/sink-blue-score-changed',
+        },
+        {
+          label: 'useSubscribeUtxosChanged',
+          to: '/subscriptions/utxos-changed',
+        },
+        {
+          label: 'useSubscribeVirtualChainChanged',
+          to: '/subscriptions/virtual-chain-changed',
+        },
+        {
+          label: 'useSubscribeVirtualDaaScoreChanged',
+          to: '/subscriptions/virtual-daa-score-changed',
+        },
+      ],
+    },
+  ],
 ]
 </script>
 
 <template>
   <div
-    class="border-l border-neutral-200 dark:border-neutral-800 divide-y divide-neutral-200 dark:divide-neutral-800 h-dvh overflow-y-auto"
+    class="border-l border-neutral-200 dark:border-neutral-800 h-dvh overflow-y-auto"
   >
+    <div class="font-bold text-xl p-4">Nuxt Kaspa Demo</div>
+    <USeparator />
     <div class="p-4">
-      <div class="font-bold text-xl">Nuxt Kaspa Demo</div>
+      <UNavigationMenu orientation="vertical" :items="items" />
     </div>
-    <DemoItem
-      v-for="item in demoItems"
-      :key="item.to"
-      :to="item.to"
-      :title="item.title"
-      :description="item.description"
-    />
   </div>
 </template>
